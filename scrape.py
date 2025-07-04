1bd2874cbf0fcd93e01681e54836d32f8549e1b6
Kheman12 <khemansaru67@gmail.com>
2025-07-04 15:15:49 +0545

    Initial Commit

---
+++ scrape.py
@@ -0,0 +1,19 @@
+from patchright.sync_api import sync_playwright
+import random 
+import time 
+
+
+def load_html(url,raw_html):
+    with sync_playwright() as p:
+        browser = p.chromium.launch_persistent_context(
+            user_data_dir=r"C:\Users\A C E R\OneDrive\Desktop\LLM powered AI Scraper\user_data",
+            channel="chrome",
+            headless=False,
+            no_viewport=True
+        )
+        page = browser.new_page()
+        page.goto(url)
+        time.sleep(random.randint(2,6))
+        raw_html = page.content()
+        return raw_html
+        browser.close()
